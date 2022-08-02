4f6c440cb19f613a1d5556fe153fbab262a971f9
Bonerix <bonerixyt@gmail.com>
2022-08-02 19:20:34 +0200

    Přidejte soubory projektu.

---
+++ Latether server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+class main
+{
+    static byte[] data;  
+    static Socket socket;
+    static void Main(string[] args)
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+        socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6666)); 
+
+        socket.Listen(1);
+        Socket accepteddata = socket.Accept();
+        data = new byte[accepteddata.SendBufferSize]; 
+        int j = accepteddata.Receive(data);
+        /*byte[] adata = new byte[j];        
+        for (int i = 0; i < j; i++)        
+            adata[i] = data[i];*/  
+        string dat = Encoding.Default.GetString(data); 
+        Console.WriteLine(dat);                         
+    }
+}
