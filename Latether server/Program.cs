d5dee81105f740117ae6cc45fea5fdd65b824675
bonerix <94288953+bonerix@users.noreply.github.com>
2022-08-03 12:20:59 +0200

    Update Program.cs

--- Latether server/Program.cs
+++ Latether server/Program.cs
@@ -13,13 +13,10 @@ class main
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
         socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.61"), 6666)); 
         
-        while (true)
-        {
             socket.Listen(1);
             Socket accepteddata = socket.Accept();
             data = new byte[accepteddata.SendBufferSize];
             string dat = Encoding.Default.GetString(data);
             Console.WriteLine(dat);
-        }
     }
 }
