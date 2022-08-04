f238cb5cd6e46f723e20ad387004be0691eb6edc
tentamten <pajamanasek@gmail.com>
2022-08-04 14:25:36 +0200

    Added sending messages to all users

--- Latether server/Program.cs
+++ Latether server/Program.cs
@@ -1,27 +1,41 @@
-using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Diagnostics;
 
-class main
+class Server
 {
-    static byte[] data;  
+    static byte[] data;
     static Socket socket;
-     
+
     static void Main(string[] args)
     {
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
-        socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.100"), 42069));
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.98"), 42069));
 
         while (true)
         {
+            //Recieving from users
             socket.Listen(1);
             Socket accepteddata = socket.Accept();
             data = new byte[accepteddata.SendBufferSize];
-            int j = accepteddata.Receive(data);
+            accepteddata.Receive(data);
             string dat = Encoding.Default.GetString(data);
-            Console.WriteLine(dat);
+
+            //Sending to users
+            IPAddress[] ips =
+            {
+                IPAddress.Parse("192.168.1.98"),
+                IPAddress.Parse("192.168.1.182"),
+                IPAddress.Parse("192.168.1.147")
+            };
+
+            foreach (IPAddress ip in ips)
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(ip, 42069);
+                s.Send(data);
+                s.Disconnect(false);
+            }
         }
     }
 }
