2608de65e8188fe1222b4d738b8c8424c62accde
Bonerix <bonerixyt@gmail.com>
2022-08-04 18:02:57 +0200

    Added error when user unavailable

--- Latether server/Program.cs
+++ Latether server/Program.cs
@@ -1,48 +1,49 @@
+using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
 class Server
 {
-    static byte[] data;
-    static Socket socket;
+	static byte[] data;
+	static Socket socket;
 
-    static void Main(string[] args)
-    {
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.61"), 42069));
+	static void Main(string[] args)
+	{
+		socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.100"), 42069));
 
-        while (true)
-        {
-            //Recieving from users
-            socket.Listen(1);
-            Socket accepteddata = socket.Accept();
-            data = new byte[accepteddata.SendBufferSize];
-            accepteddata.Receive(data);
-            string dat = Encoding.Default.GetString(data);
+		while (true)
+		{
+			//Recieving from users
+			socket.Listen(1);
+			Socket accepteddata = socket.Accept();
+			data = new byte[accepteddata.SendBufferSize];
+			accepteddata.Receive(data);
+			string dat = Encoding.Default.GetString(data);
 
-            /*//Sending to users
-            IPAddress[] ips =
-            {
-                IPAddress.Parse("192.168.1.98"),
-                IPAddress.Parse("192.168.1.182"),
-                IPAddress.Parse("192.168.1.147")
-            };
+			//Sending to users
+			IPAddress[] ips =
+			{
+				IPAddress.Parse("192.168.1.100"),
+				IPAddress.Parse("192.168.1.182"),
+				IPAddress.Parse("192.168.1.147")
+			};
 
-            foreach (IPAddress ip in ips)
-            {
-                try
-                {
-                    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    s.Connect(ip, 42069);
-                    s.Send(data);
-                    s.Disconnect(false);
-                }
-                catch
-                {
-
-                }
-            }*/
-        }
-    }
+			foreach (IPAddress ip in ips)
+			{
+				try
+				{
+					Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+					s.Connect(ip, 42069);
+					s.Send(data);
+					s.Disconnect(false);
+				}
+				catch
+				{
+                    Console.WriteLine("Error: Client on ip {0} unavailable.", ip);
+				}
+			}
+		}
+	}
 }
